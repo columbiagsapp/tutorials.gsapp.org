066b9413d1c9531e81886cfdad82afcfbf4d15d4
Troy Conrad Therrien <troyth@gmail.com>
2012-10-30 13:34:18 -0400

    changed .delegate(selector) to .find(selector).bind() because it seems .delegate(selector) doesn't cull the selector from the parent, $(this)

--- sites/all/libraries/backbone/backbone.js
+++ sites/all/libraries/backbone/backbone.js
@@ -1235,7 +1235,8 @@
         if (selector === '') {
           this.$el.bind(eventName, method);
         } else {
-          this.$el.delegate(selector, eventName, method);
+          //this.$el.delegate(selector, eventName, method);
+          this.$el.find(selector).bind(eventName, method);//tct2003 changed because delegate doesn't search through $(this)
         }
       }
     },
