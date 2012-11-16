aa69ffcd4e93832448a14a571b913d8f477ab39e
jh mpbnew osx <vomc8one@gmail.com>
2012-11-15 20:20:55 -0500

    checking in framework which contains the services specific code for node creation etc

---
+++ sites/all/themes/framework/node--tutorial.tpl.php
@@ -0,0 +1,40 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && $title): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+      <span class="submitted">
+        <?php print $user_picture; ?>
+        <?php print $submitted; ?>
+      </span>
+    <?php endif; ?>
+  </header>
+
+  <?php
+    // Hide comments, tags, and links now so that we can render them later.
+
+    print 'blablabla';
+    print render($content);
+
+
+    print 'hit this to save a test question <a href="#" id="add-question">add a new node</a>';
+
+    //print render($content);
+  ?>
+
+  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
+    <footer>
+      <?php print render($content['field_tags']); ?>
+      <?php print render($content['links']); ?>
+    </footer>
+  <?php endif; ?>
+
+  <?php print render($content['comments']); ?>
+
+</article> <!-- /.node -->
---
+++ sites/all/themes/framework/tutorial-d7.js
@@ -0,0 +1,83 @@
+jQuery(document).ready(function ($) {
+
+  
+
+  console.log('doc ready');
+  
+
+$('#add-question').click(function() {
+
+
+      // make new node
+      var node_array = {
+        'title': 'new title',
+        'type': 'question',
+        'body': 'some question'
+      };
+
+      // json encode it
+      var json_node = JSON.stringify(node_array);
+      console.log('json node made');
+      console.dir(json_node);
+
+
+
+      // write it to question_save
+      $.ajax({
+        type: 'POST',
+        /*
+        endpoint testing
+        defined in services as 'question_save'
+
+        url             result
+        question_save   http://tutorials-test7.postfog.org/node/question_save = 404
+
+        */
+
+        url: '/question-save/node', // the path / hook to hit
+        dataType: 'json',
+        success: updateData,
+        contentType: "application/json;charset=utf-8",
+        error: postError,
+        data: json_node
+      });
+      
+      console.log('posted');
+
+
+
+
+      // see what comes back
+       var updateData = function(data) {
+        // log successloghtml = loghtml + '<br/>Success!';
+        // log something or give confirm message
+        console.log('success');
+        console.log(data);
+      }
+
+      // gets called on error
+      var postError = function(jqXHR, errText, errThrown) {
+        // write an error and handle it
+        console.log('error');
+        console.log(errText);
+        console.log(errThrown);
+
+      }
+
+  });
+
+
+
+
+  //var xNode = Drupal.Backbone.Models.Node({nid: 55});
+
+
+  //console.log(xNode);
+
+
+
+
+
+
+
+});
