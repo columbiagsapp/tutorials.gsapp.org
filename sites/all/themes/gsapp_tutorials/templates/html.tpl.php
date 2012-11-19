8563ddd893f68583f26f064561252bc4408fe61a
Troy Conrad Therrien <troyth@gmail.com>
2012-11-19 12:23:20 -0500

    created subtheme using Twitter Bootstrap example

---
+++ sites/all/themes/gsapp_tutorials/templates/html.tpl.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+
+
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+  <!-- Fav and touch icons -->
+  <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
